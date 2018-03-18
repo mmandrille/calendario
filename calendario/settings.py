564ec7e5843602d7f18cc0d2c5eb231d888e36a2
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-03-18 17:24:46 -0300

    Se realizaron los cambios iniciales para empezar a trabajar en el proyecto

--- calendario/settings.py
+++ calendario/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Apps Instaladas
+
+    #Apps Propias
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -102,19 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-ar'
+TIME_ZONE = 'America/Argentina/Jujuy'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
