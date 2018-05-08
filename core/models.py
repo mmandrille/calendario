6c6a1061df1994f00e13d0b76f6f32fc2a1219d4
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-05-08 11:57:31 -0300

    se avanza sobre el desarrollo!

--- core/models.py
+++ core/models.py
@@ -7,9 +7,9 @@ from django.contrib.auth.models import *
 class Evento(models.Model):
     nombre = models.CharField('Titulo', max_length=200)
     descripcion = models.TextField()
-    fecha = models.DateTimeField('Fecha del Evento')
+    fecha_inicio = models.DateTimeField('Fecha del Evento', default=datetime.datetime.now())
+    fecha_fin = models.DateTimeField('Fecha de fin del Evento', default=datetime.datetime.now())
     models.ForeignKey(User, on_delete=models.CASCADE)
     #ubicacion = ciudades? 
-    pub_date = models.DateTimeField('Fecha de Publicacion', default=datetime.datetime.now())
     def __str__(self):
         return self.nombre
--- core/views.py
+++ core/views.py
@@ -6,5 +6,5 @@ from .models import Evento
 
 # Create your views here.
 def calendario(request):
-    eventos = Evento.objects.order_by('fecha')
+    eventos = Evento.objects.order_by('fecha_inicio')
     return render(request, 'home.html', { 'eventos': eventos, 'hoy' : datetime.now().strftime('%d/%m/%Y')})
