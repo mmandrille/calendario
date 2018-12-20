0eb932564e62f7b114fec5df9bd42ad4c7888ef0
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-12-20 16:54:33 -0300

    Se emprolijo el sistema

--- core/models.py
+++ core/models.py
@@ -1,11 +1,25 @@
 from __future__ import unicode_literals
-from django.db import models
 import datetime
-from django.contrib.auth.models import *
+
+from django.db import models
+from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+#Para api
+import requests 
+import json
+
+#Funciones API
+def obtener_organismos():
+	r = requests.get('http://organigrama.jujuy.gob.ar/ws_org/')
+	orgs = json.loads(r.text)['data']
+	organismos = list()
+	for org in orgs:
+		organismos.append((org['id'],org['nombre']))
+	return organismos
 
 #Create your models here.
 class Evento(models.Model):
+    organismo = models.PositiveIntegerField(choices= obtener_organismos(), default=0)
     nombre = models.CharField('Titulo', max_length=200)
     descripcion = HTMLField()
     fecha_inicio = models.DateTimeField('Fecha del Evento', default=datetime.datetime.now)
