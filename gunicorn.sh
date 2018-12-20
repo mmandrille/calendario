bd69b9410977fe9cc307bb2b39a98039f6abfe0b
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-12-20 19:37:43 -0300

    Se modificaron pavadas.

--- gunicorn.sh
+++ gunicorn.sh
@@ -3,4 +3,3 @@ cd /opt/calendario
 source venv/bin/activate
 cd /opt/calendario/calendario
 gunicorn calendario.wsgi -t 600 -b 127.0.0.1:8002 -w 6 --user=servidor --group=servidor --log-file=/opt/calendario/gunicorn.log 2>>/opt/calendario/gunicorn.log
-
